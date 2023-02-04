ab087332f8355188899422c22de2c3ee40d51fd4
constvict <constantinumnov@gmail.com>
2023-02-04 20:58:10 +0700

    back to working version

--- src/diffTreeGenerator.js
+++ src/diffTreeGenerator.js
@@ -3,21 +3,21 @@ import _ from 'lodash';
 const getDiffTree = (data1, data2) => {
   const sortedKeys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
   return sortedKeys.map((key) => {
-    const diffTree = {};
     if (!Object.hasOwn(data1, key)) {
-      Object.assign(diffTree, { type: 'added', key, value: data2[key] });
-    } else if (!Object.hasOwn(data2, key)) {
-      Object.assign(diffTree, { type: 'removed', key, value: data1[key] });
-    } else if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-      Object.assign(diffTree, { type: 'nested', key, children: getDiffTree(data1[key], data2[key]) });
-    } else if (_.isEqual(data1[key], data2[key])) {
-      Object.assign(diffTree, { type: 'equal', key, value: data1[key] });
-    } else {
-      Object.assign(diffTree, {
-        type: 'modified', key, dataValue1: data1[key], dataValue2: data2[key],
-      });
+      return { type: 'added', key, value: data2[key] };
     }
-    return diffTree;
+    if (!Object.hasOwn(data2, key)) {
+      return { type: 'removed', key, value: data1[key] };
+    }
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      return { type: 'nested', key, children: getDiffTree(data1[key], data2[key]) };
+    }
+    if (_.isEqual(data1[key], data2[key])) {
+      return { type: 'equal', key, value: data1[key] };
+    }
+    return {
+      type: 'modified', key, dataValue1: data1[key], dataValue2: data2[key],
+    };
   });
 };
 
